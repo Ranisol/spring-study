8da2d343fafc52c24a6d547c8153a265f27fe9ba
Ranisol <gim883364@gmail.com>
2022-01-09 03:03:23 +0900

    project init

---
+++ src/main/kotlin/com/personalstudy/aboutjpa/AboutJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.personalstudy.aboutjpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AboutJpaApplication
+
+fun main(args: Array<String>) {
+	runApplication<AboutJpaApplication>(*args)
+}
---
+++ src/test/kotlin/com/personalstudy/aboutjpa/AboutJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.personalstudy.aboutjpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AboutJpaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
